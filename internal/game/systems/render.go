8d72f1b4bda0125552f9660011db213f2d743b35
usysrc <usysrc@usysrc.dev>
2024-11-17 18:31:34 +0100

    refactor: :fire: removed unused code

--- internal/game/systems/render.go
+++ internal/game/systems/render.go
@@ -33,12 +33,6 @@ func (r *RenderSystem) Draw() {
 	for _, data := range renderData {
 		if pos, ok := r.Components.Positions[data.entity]; ok {
 			// Draw the entity's image at its position
-			//options := &raylib.DrawImageOptions{}
-			//options.GeoM.Reset()
-			//options.GeoM.Scale(data.render.Scale, data.render.Scale)
-			//options.GeoM.Translate(pos.X, pos.Y)
-
-			//rl.DrawTexture(data.render.Image, int32(pos.X), int32(pos.Y), rl.White)
 			rl.DrawTextureEx(data.render.Image, rl.Vector2{X: float32(pos.X), Y: float32(pos.Y)}, 0, float32(data.render.Scale), rl.White)
 		}
 	}
